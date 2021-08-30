3286ccb2c830f8e134e14665ea0b34445edafcc2
djunushbaev5 <rus.dzhunushbaev.03@mail.ru>
2021-08-30 15:01:01 +0300

    танк

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,13 @@ import java.util.Random;
 
 public class Main {
 
-    public static int bristlebackHealth = 1000; //Здоровье босса
-    public static int bristlebackDamage = 40; //Урон босса
+    public static int bristlebackHealth = 4000; //Здоровье босса
+    public static int bristlebackDamage = 50; //Урон босса
     public static String bristlebackDefenceType = " "; //Защита босса
-    public static int[] heroesHealth = {400, 400, 400, 400}; //Здоровье героев
-    public static int[] heroesDamage = {40, 0, 40, 40}; //Урон героев
-    public static int doctor = 15; //Доктор
-    public static String[] heroesAtackType = {"Invoker", "Doctor", "Sniper", "Anti-Mage"}; //Герои
+    public static int[] heroesHealth = {450, 250, 350, 500, 600}; //Здоровье героев
+    public static int[] heroesDamage = {40, 0, 40, 40, 10}; //Урон героев
+    public static int doctor = 20; //Доктор
+    public static String[] heroesAtackType = {"Invoker", "Doctor", "Sniper", "Anti-Mage", "Tank"}; //Герои
 
     public static void main(String[] args) {
         fightInfo();
@@ -21,13 +21,16 @@ public class Main {
 
     public static void round() {   //Параметры раунда
         changeBristlebackDefence();
-        brislebackHit();
         if (bristlebackHealth > 0) {
-            heroesHit();
+            brislebackHit();
         }
+        heroesHit();
+
+
         fightInfo();
         hill();
     }
+
 
     public static boolean isFinished() {   //Победа босса, Героев
         if (bristlebackHealth <= 0) {
@@ -35,16 +38,36 @@ public class Main {
             return true;
         }
 
-        if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0 && heroesHealth[3] <= 0) {
+        if (heroesHealth[0] <= 0 && heroesHealth[1] <= 0 && heroesHealth[2] <= 0 && heroesHealth[3] <= 0 && heroesHealth[4] <= 0) {
             System.out.println("Bristleback won!");
             return true;
         }
         return false;
     }
 
-    public static void hill() { //Лечение
+    public static void tank() {
         for (int i = 0; i < heroesHealth.length; i++) {
-            heroesHealth[i] = heroesHealth[i] + doctor;
+            if (heroesHealth[4] - bristlebackDamage / 2 < 0) {
+                heroesHealth[4] = 0;
+            } else {
+                heroesHealth[4] -= bristlebackDamage / 2;
+            }
+            if (heroesHealth[i] - bristlebackDamage / 2 < 0) {
+                heroesHealth[i] = 0;
+            } else {
+                heroesHealth[i] = heroesHealth[i] - bristlebackDamage / 2;
+            }
+        }
+
+    }
+
+    public static void hill() { //Лечение
+        if (heroesHealth[3] > 0) {
+            for (int i = 0; i < heroesHealth.length; i++) {
+                if (heroesHealth[i] > 0) {
+                    heroesHealth[i] = heroesHealth[i] + doctor;
+                }
+            }
         }
     }
 
@@ -56,8 +79,17 @@ public class Main {
 
 
     public static void brislebackHit() {  //Босс наносит урон
-        for (int i = 0; i < heroesHealth.length; i++) {
-            heroesHealth[i] = heroesHealth[i] - bristlebackDamage;
+        if (heroesHealth[4] > 0) {
+            tank();
+        } else {
+            for (int i = 0; i < heroesHealth.length; i++) {
+                if (heroesHealth[i] - bristlebackDamage < 0) {
+                    heroesHealth[i] = 0;
+                }
+                if (heroesHealth[i] > 0) {
+                    heroesHealth[i] = heroesHealth[i] - bristlebackDamage;
+                }
+            }
         }
     }
 
@@ -86,11 +118,12 @@ public class Main {
 
     public static void fightInfo() {  //Информация сражения
         System.out.println("______________");
-        System.out.println("Bristleback Health " + bristlebackHealth);
-        System.out.println("Invoker Health " + heroesHealth[0]);
-        System.out.println("Crystall Maden Health " + heroesHealth[1]);
-        System.out.println("Sniper Health " + heroesHealth[2]);
-        System.out.println("Anti-Mage Health " + heroesHealth[3]);
+        System.out.println("Bristleback Health: " + bristlebackHealth);
+        System.out.println("Invoker Health: " + heroesHealth[0]);
+        System.out.println("Doctor Health: " + heroesHealth[1]);
+        System.out.println("Sniper Health: " + heroesHealth[2]);
+        System.out.println("Anti-Mage Health: " + heroesHealth[3]);
+        System.out.println("Tank Health: " + heroesHealth[4]);
         System.out.println("______________");
     }
 }
